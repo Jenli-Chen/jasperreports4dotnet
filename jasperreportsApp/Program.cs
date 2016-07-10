6463ebddbbc3feb1832acdebd88fd688727a3fb9
Jenli Chen(陳人立) <Jenli_Chen@asus.com>
2016-07-10 15:01:28 +0800

    add examples

--- jasperreportsApp/Program.cs
+++ jasperreportsApp/Program.cs
@@ -10,7 +10,22 @@ namespace jasperreportsApp
     {
         static void Main(string[] args)
         {
-            //
+            DSTest sqlconn_test = new SqlConnectionDSTest();
+            sqlconn_test.ExpReort(DSTest.TASK_PDF);
+            sqlconn_test.ExpReort(DSTest.TASK_DOCX); 
+            sqlconn_test.ExpReort(DSTest.TASK_XLSX);
+            sqlconn_test.ExpReort(DSTest.TASK_PPTX);
+            sqlconn_test.ExpReort(DSTest.TASK_RTF);
+            sqlconn_test.ExpReort(DSTest.TASK_HTML);
+
+            DSTest jrtable_test = new JRTableModelDSTest(); 
+            jrtable_test.ExpReort(DSTest.TASK_PDF);
+            jrtable_test.ExpReort(DSTest.TASK_DOCX);
+            jrtable_test.ExpReort(DSTest.TASK_XLSX);
+            jrtable_test.ExpReort(DSTest.TASK_PPTX);
+            jrtable_test.ExpReort(DSTest.TASK_RTF);
+            jrtable_test.ExpReort(DSTest.TASK_HTML);
+
         }
     }
 }
